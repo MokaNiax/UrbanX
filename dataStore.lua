c4ce6e0da3478ab5de320ec25e0dfe4b2197f703
MokaNiax <87872507+MokaNiax@users.noreply.github.com>
2025-02-19 13:48:31 +0100

    Update dataStore.lua

--- dataStore.lua
+++ dataStore.lua
@@ -1,6 +1,6 @@
 local root = script.Parent.Parent
 local dataStore = require(root.modules.dataStore)
 
-datastore:setUp(player) -- DON'T USE IT, it used to setup player value !
-datastore:saveData(player) -- Used to save player data.
-datastore:loadData(player) -- Used to load player data.
+dataStore:setUp(player) -- DON'T USE IT, it used to setup player value !
+dataStore:saveData(player) -- Used to save player data.
+dataStore:loadData(player) -- Used to load player data.
