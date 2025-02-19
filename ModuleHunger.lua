5bde7e95a1b4d65fdaa1ca08a2030a49ef31f240
MokaNiax <87872507+MokaNiax@users.noreply.github.com>
2025-02-19 11:58:27 +0100

    Delete ModuleHunger.lua

--- ModuleHunger.lua
+++
@@ -1,7 +0,0 @@
-local hunger = require(script.Parent.Parent.modules.hunger)
-
-local amountExemple = 20
-
-remotepath.OnServerEvent:Connect(function(player)
-  hunger.eat(player, amountExemple)
-end)
