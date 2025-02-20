014f09adfaf4d79a906ed764812194d5ca67a7de
MokaNiax <87872507+MokaNiax@users.noreply.github.com>
2025-02-20 21:08:40 +0100

    Update dataStore.lua

--- dataStore.lua
+++ dataStore.lua
@@ -7,3 +7,5 @@ dataStore:setUp(player) -- DON'T USE IT, it used to setup player value !
 dataStore:saveData(player) -- Used to save player data.
 dataStore:loadData(player) -- Used to load player data.
 dataStore:changePlayerValue(playerID, valueName, newValue) -- Edit data of a disconnected player.
+
+-- All default data : "money", "bank", "hunger", "job", "job_rank", "name", "perm_lvl", "banned"
