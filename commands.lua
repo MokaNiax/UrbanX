1ffa7c4f3bf354dd7163f1e98ec7f89fb4509f92
MokaNiax <87872507+MokaNiax@users.noreply.github.com>
2025-02-20 15:18:23 +0100

    Update commands.lua

--- commands.lua
+++ commands.lua
@@ -3,6 +3,6 @@ local replicatedStorage = game:GetService("ReplicatedStorage")
 local root = replicatedStorage:WaitForChild("UrbanX")
 local commands = require(root.modules.commands)
 
-commands:createCommand(name, desc, func) -- Used to register a tchat command.
+commands:createCommand(name, permLvl, func) -- Used to register a tchat command, permLvl = 0 permit everyone to use the command.
 commands:executeCommand(name, player, ...) -- Used to execute a tchat command.
 commands:listCommands() -- Used to list all command.
